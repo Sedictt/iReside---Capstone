03158de29bbb2dc29f868cc3e1d3b11e2ed011b7
iReside Mobile Dev <mobile-dev@ireside.app>
2026-09-10 16:17:25 +0800

    fix(mobile): increase bottom scroll padding across views so Revenue Trend and bottom cards are fully visible above tab bar

--- src/components/mobile/landlord/LandlordOverviewView.tsx
+++ src/components/mobile/landlord/LandlordOverviewView.tsx
@@ -136,7 +136,7 @@ export function LandlordOverviewView() {
 
     return (
         <PullToRefresh onRefresh={fetchData}>
-            <div className="flex flex-col gap-4 pb-6">
+            <div className="flex flex-col gap-4 pb-28">
                 {/* Prominently Highlighted Property Dropdown Selector */}
                 <div className="px-4 pt-1">
                     <div className="relative w-full">
--- src/components/mobile/landlord/LandlordTicketsView.tsx
+++ src/components/mobile/landlord/LandlordTicketsView.tsx
@@ -130,7 +130,7 @@ export function LandlordTicketsView() {
 
     return (
         <PullToRefresh onRefresh={fetchTickets}>
-            <div className="flex flex-col gap-3.5 pb-6">
+            <div className="flex flex-col gap-3.5 pb-28">
                 {/* Search Bar */}
                 <div className="px-4 pt-1 flex items-center">
                     <div className="relative flex-1">
